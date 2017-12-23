35907a16f6b6308fd07030bfaebb38bc7850ba1c
Deepanshu Chauhan <deepansuchauhan.02@gmail.com>
2017-12-23 21:56:11 +0530

    Add GET /users/me  and authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -30,15 +30,13 @@ var UserSchema = new mongoose.Schema({
       required: true
     }
   }]
-},{
-  usePushEach : true
 });
 
-UserSchema.methods.toJSON = function(){
+UserSchema.methods.toJSON = function () {
   var user = this;
   var userObject = user.toObject();
 
-  return _.pick(userObject,['_id','email']);
+  return _.pick(userObject, ['_id', 'email']);
 };
 
 UserSchema.methods.generateAuthToken = function () {
@@ -46,10 +44,27 @@ UserSchema.methods.generateAuthToken = function () {
   var access = 'auth';
   var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
-  user.tokens.unshift({access, token});
+  user.tokens.push({access, token});
 
-     return user.save().then(() => {
+  return user.save().then(() => {
     return token;
+  });
+};
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
   });
 };
 
